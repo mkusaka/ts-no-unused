a3fceb541b9494254289fee3328b419c9ff4b2ba
mkusaka <hinoshita1992@gmail.com>
2021-07-29 01:16:01 +0900

    feat: implement cli

---
+++ src/cli.ts
@@ -0,0 +1,53 @@
+import meow from "meow";
+import { join } from "path";
+import { Project } from "ts-morph";
+
+const consoleLog =
+  (level: string) =>
+  (...logs: any[]) => {
+    if (level === "none") {
+      return;
+    }
+    console.log(...logs);
+  };
+
+const cli = meow(
+  `
+  Usage
+    $ npx no-unused
+    $ npx on-unused --tsconfig-path ./src/tsconfig.json
+    $ npx on-unused --logLevel debug`,
+  {
+    importMeta: import.meta,
+    flags: {
+      tsconfigPath: {
+        type: "string",
+        default: join(process.cwd(), "tsconfig.json"),
+        description: "Path to tsconfig.json",
+      },
+      logLevel: {
+        type: "string",
+        default: "none",
+        description: "Log level",
+      },
+    },
+  }
+);
+
+const tsconfigPath = cli.flags.tsconfigPath;
+const logLevel = cli.flags.logLevel;
+const logger = consoleLog(logLevel);
+
+const project = new Project({
+  tsConfigFilePath: tsconfigPath,
+});
+
+const sourceFiles = project.getSourceFiles();
+
+for (const sourceFile of sourceFiles) {
+  logger(`start remove: ${sourceFile.getFilePath()} unused identifier`);
+  sourceFile.fixUnusedIdentifiers();
+  sourceFile.saveSync();
+  logger(`done remove: ${sourceFile.getFilePath()} unused identifier`);
+}
+
